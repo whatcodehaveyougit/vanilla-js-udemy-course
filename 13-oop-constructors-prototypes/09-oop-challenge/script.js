d2151cb672a7b860260f4110ea478d58f84294d2
whatcodehaveyougit <sigurdwatt@gmail.com>
2024-03-14 15:01:08 +0000

    Add and remove meal/workout working

--- 13-oop-constructors-prototypes/09-oop-challenge/script.js
+++ 13-oop-constructors-prototypes/09-oop-challenge/script.js
@@ -1,35 +1,27 @@
-function Player(name) {
+function Player(){
   this.name = name;
   this.lvl = 1;
   this.points = 0;
 }
 
-Player.prototype.gainXp = function (xp) {
+// Player.prototype = Object.create(Player.prototype);
+
+Player.prototype.gainXp = function(xp){
+  // console.log(xp)
   this.points += xp;
 
-  if (this.points >= 10) {
+  if (this.points > 10){
+    this.points = 0;
     this.lvl++;
-    this.points -= 10;
   }
+  this.describe()
+}
 
-  console.log(this.describe());
-};
-
-Player.prototype.describe = function () {
-  return `${this.name} is level ${this.lvl} with ${this.points} experience points`;
-};
+Player.prototype.describe = function(){
+  console.log(`${this.name} is level ${this.lvl} with ${this.points} experience points`);
+}
 
 const player1 = new Player('Bob');
-const player2 = new Player('Alice');
-
-player1.gainXp(4);
-player2.gainXp(7);
-player1.gainXp(5);
-player2.gainXp(1);
-player1.gainXp(7);
-player2.gainXp(9);
-player1.gainXp(5);
-player2.gainXp(2);
-
-// console.log(player1.describe());
-// console.log(player2.describe());
+player1.gainXp(1)
+player1.gainXp(10)
+player1.gainXp(10)
